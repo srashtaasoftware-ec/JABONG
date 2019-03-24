293ff118170513b529b9296ebd3fa2d161060d18
Sandhya Vemala <sandhya.vemala@ericsson.com>
2019-03-24 22:28:05 +0530

    adding to remote area

---
+++ utilities.java
@@ -0,0 +1,3 @@
+
+simple text iam doing here
+SRM reports this list as a feed from MAINZ to Exasol and Exasol takes this list of users the geographic coordinates based on LOCID. Exasol calculates the distances of the customer pharmacies to all panel Pharmacies and selects the 25 nearest pharmacies with distance> 0 per customer pharmacy. These 25 pharmacies panel form the "Trade Area" of the pharmacy. The exclusion criteria for PZNs are carried out before the TOP selection. For each pharmacy TOP 3000 Non-Rx PZNs are determined based on the business rules. If less than 3000 PZNs are found all are taken.
